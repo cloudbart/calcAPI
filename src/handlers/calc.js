de2becc0bdcd849c31027c07030e9cc3b37b9ffc
CloudBart <56924627+cloudbart@users.noreply.github.com>
2020-09-29 13:30:42 -0400

    Update calc.js

--- src/handlers/calc.js
+++ src/handlers/calc.js
@@ -1,22 +1,23 @@
-console.log('Loading the Calc function');
+console.log('Loading the Calculator function');
 
 exports.handler = function(event, context, callback) {
+    console.log('Invoked ARN: ', context.invokedFunctionArn);
+    console.log('Function Version: ', context.functionVersion);
     console.log('Received event:', JSON.stringify(event, null, 2));
     if (event.a === undefined || event.b === undefined || event.op === undefined) {
-        callback(new Error("400 Invalid Input"));
+        return callback(new Error("400 Invalid Input"));
     }
-    
+
     var result = {};
     result.a = Number(event.a);
     result.b = Number(event.b);
     result.op = event.op;
-    
+
     if (isNaN(event.a) || isNaN(event.b)) {
-        callback(new Error("400 Invalid Operand"));
+        return callback(new Error("400 Invalid Operand"));
     }
 
-    switch(event.op)
-    {
+    switch (event.op) {
         case "+":
             result.c = result.a + result.b;
             break;
@@ -27,12 +28,17 @@ exports.handler = function(event, context, callback) {
             result.c = result.a * result.b;
             break;
         case "/":
-            result.c = result.b===0 ? NaN : Number(event.a) / Number(event.b);
+            result.c = result.b === 0 ? NaN : Number(event.a) / Number(event.b);
             break;
         default:
-            callback(new Error("400 Invalid Operator"));
-            break;
+            return callback(new Error("400 Invalid Operator"));
     }
     console.log('Calculation results:', JSON.stringify(result, null, 2));
-    callback(null, result);
+    var response = {};
+    response.InvokedARN = context.invokedFunctionArn;
+    response.InvokedVersion = context.functionVersion;
+    response.a = Number(event.a);
+    response.b = Number(event.b);
+    response.op = event.op;
+    callback(null, response);
 };
