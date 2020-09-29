ac35eb5a9a0c73f5eeada05d2193286416568495
CloudBart <56924627+cloudbart@users.noreply.github.com>
2020-09-29 13:32:04 -0400

    Update calc.js

--- src/handlers/calc.js
+++ src/handlers/calc.js
@@ -3,7 +3,7 @@ console.log('Loading the Calculator function');
 exports.handler = function(event, context, callback) {
     console.log('Invoked ARN: ', context.invokedFunctionArn);
     console.log('Function Version: ', context.functionVersion);
-    console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('Received calculation event:', JSON.stringify(event, null, 2));
     if (event.a === undefined || event.b === undefined || event.op === undefined) {
         return callback(new Error("400 Invalid Input"));
     }
@@ -34,6 +34,7 @@ exports.handler = function(event, context, callback) {
             return callback(new Error("400 Invalid Operator"));
     }
     console.log('Calculation results:', JSON.stringify(result, null, 2));
+    
     var response = {};
     response.InvokedARN = context.invokedFunctionArn;
     response.InvokedVersion = context.functionVersion;
